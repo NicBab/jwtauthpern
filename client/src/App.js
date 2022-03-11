51b4c5078cd8dbb6faadf7151425ca4749de0ba2
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-03-10 18:05:06 -0600

    build register component and set token to local storage/ building login component similar

---
+++ client/src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Dashboard, Login, Register } from './components/index'
+
+function App () {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean)
+  }
+
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/login"
+              element={
+                isAuthenticated ? (
+                  <Dashboard setAuth={setAuth} />
+                ) : (
+                  <Login setAuth={setAuth} />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              element={
+                !isAuthenticated ? (
+                  <Register setAuth={setAuth} />
+                ) : (
+                  <Dashboard setAuth={setAuth} />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              element={
+                isAuthenticated ? (
+                  <Dashboard setAuth={setAuth} />
+                ) : (
+                  <Login setAuth={setAuth} />
+                )
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+
+
+            // <Route
+            //   path="/login"
+            //   render={(props) => 
+            //     !isAuthenticated ? (
+            //       <Login {...props} />
+            //     ) : (
+            //       <Navigate to="/dashboard" />
+            //     )
+            //   }
+            // />
+
+            // <Route
+            //   path="/register"
+            //   render={(props) =>
+            //     !isAuthenticated ? (
+            //       <Register {...props} />
+            //     ) : (
+            //       <Navigate to="/login" />
+            //     )
+            //   }
+            // />
+
+            // <Route
+            //   path="/dashboard"
+            //   render={(props) =>
+            //     !isAuthenticated ? (
+            //       <Dashboard {...props} />
+            //     ) : (
+            //       <Navigate to="/login" />
+            //     )
+            //   }
+            // /> 
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
-export { Default as Dashboard } from './Dashboard'
-export { Default as Login } from './Login'
-export { Default as Register } from './Register'
+export { default as Dashboard } from './Dashboard'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
